708a45e45c46792d9543a081291e4ad213d0a695
Sergei Gordeev <gordeser@cvut.cz>
2023-02-01 23:01:21 +0100

    Project 10: add condition to a component

--- 10-conditional-rendering/src/App.js
+++ 10-conditional-rendering/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import PetInfo from "./components/PetInfo";
 function App() {
-  return (
-    <div className="App">
-      <PetInfo animal="cat" age="10" />
-    </div>
-  );
+    return (
+        <div className="App">
+            <PetInfo animal="cat" age="10" hasPet />
+            <PetInfo hasPet={false} />
+        </div>
+    );
 }
 
 export default App;
