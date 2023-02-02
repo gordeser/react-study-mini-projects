28381721a00758ee33d29d4c04dab8e2b55c7863
Sergei Gordeev <gordeser@cvut.cz>
2023-02-02 14:20:42 +0100

    Project 11: add function to generate random numbers and add it to the component

--- 11-state/src/components/RandomNumber.js
+++ 11-state/src/components/RandomNumber.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
+function generateRandomNum() {
+    return Math.floor(Math.random() * 100)
+}
+
 function RandomNumber() {
+    const [randomNum, setRandomNum] = useState(generateRandomNum())
+
+    const changeRandomNum = () => setRandomNum(generateRandomNum())
+
     return (
         <div>
-            <h1>100</h1>
-            <button>Generate new random number</button>
+            <h1>{randomNum}</h1>
+            <button onClick={changeRandomNum}>Generate new random number</button>
         </div>
     )
 }
