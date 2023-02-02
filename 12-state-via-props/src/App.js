ee461cd1aa9878dec21a658b832c3eb277024c49
Sergei Gordeev <gordeser@cvut.cz>
2023-02-02 15:51:05 +0100

    Project 12: new count state for counter

--- 12-state-via-props/src/App.js
+++ 12-state-via-props/src/App.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import './App.css';
 import Button from "./components/Button";
 import Counter from "./components/Counter";
 function App() {
-  return (
+    const [count, setCount] = useState(0)
+    return (
     <div className="App">
-      <Counter />
+      <Counter count={count}/>
       <Button />
       <Button />
       <Button />
