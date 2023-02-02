9fb8a3aeeb98554d0adb659a6f9d251030f5ef92
Sergei Gordeev <gordeser@cvut.cz>
2023-02-02 14:00:34 +0100

    Project 11: add new component RandomNumber

---
+++ 11-state/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import RandomNumber from "./components/RandomNumber";
+
+
+function App() {
+  return (
+    <div className="App">
+      <RandomNumber />
+    </div>
+  );
+}
+
+export default App;
