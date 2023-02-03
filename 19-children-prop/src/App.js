fadac3def06bbf343bd03f6037c3d4ba0302e8c8
Sergei Gordeev <gordeser@cvut.cz>
2023-02-03 19:37:40 +0100

    Project 19: add component wrapper

--- 19-children-prop/src/App.js
+++ 19-children-prop/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+        <Wrapper color="lightblue">
+            <h2>Text inside the wrapper</h2>
+            <button>Click me!</button>
+        </Wrapper>
+        <Wrapper color="lightgreen">
+            <h2>Another text</h2>
+            <p>Some description</p>
+            <input type="text" placeholder="Enter value" />
+        </Wrapper>
+    </div>
   );
 }
 
