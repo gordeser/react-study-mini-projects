6c37e4117d715ed97790ae1c0ef2bd85353b71f2
Sergei Gordeev <gordeser@cvut.cz>
2023-02-03 18:04:02 +0100

    Project 16: add card component

--- 16-react-fragment/src/App.js
+++ 16-react-fragment/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Card from "./Components/Card";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Card />
+    </div>
   );
 }
 
---
+++ 16-react-fragment/src/Components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <div>
+            <h1>Some name</h1>
+            <h3>Some text</h3>
+            <button>Some button</button>
+        </div>
+    )
+}
+
+export default Card
