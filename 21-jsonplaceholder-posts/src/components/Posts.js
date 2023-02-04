140ab373832cfce9309c82322089157cb0d9a4d6
Sergei Gordeev <gordeser@cvut.cz>
2023-02-04 22:58:31 +0100

    Project 21: get posts from jsonplaceholder

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -1,6 +1,15 @@
 import Post from "./Post";
+import {useEffect, useState} from "react";
 
 function Posts() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setPosts(json))
+    }, [])
+
     return (
         <div>
             <Post />
