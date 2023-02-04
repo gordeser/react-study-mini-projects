40d0d99dfc715c9adcac0dc81041bfaa31908b01
Sergei Gordeev <gordeser@cvut.cz>
2023-02-04 23:27:14 +0100

    Project 21: showing if there is an error

--- 21-jsonplaceholder-posts/src/components/Posts.js
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -3,13 +3,18 @@ import {useEffect, useState} from "react";
 
 function Posts() {
     const [posts, setPosts] = useState([])
+    const [error, setError] = useState('')
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(res => res.json())
             .then(posts => setPosts(posts))
-            .catch(error => console.log(error.message))
+            .catch(error => setError(error.message))
     }, [])
+
+    if (error) {
+        return <h1>Error: {error}</h1>
+    }
 
     return (
         <div>
