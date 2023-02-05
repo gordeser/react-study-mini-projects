00b2edd028d4d96f492466cac4eaccd3b7ae519a
Sergei Gordeev <gordeser@cvut.cz>
2023-02-05 18:43:03 +0100

    Project 25: add new buttons and styles

--- 25-todo-app-v2/src/components/Todos/Todo.js
+++ 25-todo-app-v2/src/components/Todos/Todo.js
@@ -1,11 +1,14 @@
 import styles from './Todo.module.css'
-import { RiTodoFill } from "react-icons/ri";
+import {RiDeleteBin2Line, RiTodoFill} from "react-icons/ri";
+import {FaCheck} from "react-icons/fa";
 
 function Todo({todo, deleteTodo}) {
     return (
         <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
             <RiTodoFill className={styles.todoIcon} />
-            <div className={styles.todoText}>{todo.text}</div>
+            <div className={styles.todoText}>{todo.text} </div>
+            <RiDeleteBin2Line className={styles.deleteIcon} />
+            <FaCheck className={styles.checkIcon} />
         </div>
     )
 }
