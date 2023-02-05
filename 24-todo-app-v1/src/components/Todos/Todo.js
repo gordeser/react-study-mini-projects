6b152a4084a3b85881959c88865897e520be59e9
Sergei Gordeev <gordeser@cvut.cz>
2023-02-05 17:22:31 +0100

    Project 24: add icons before todo text

--- 24-todo-app-v1/src/components/Todos/Todo.js
+++ 24-todo-app-v1/src/components/Todos/Todo.js
@@ -1,8 +1,10 @@
 import styles from './Todo.module.css'
+import { RiTodoFill } from "react-icons/ri";
 
 function Todo({todo, index, deleteTodo}) {
     return (
         <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+            <RiTodoFill className={styles.todoIcon} />
             <div className={styles.todoText}>{todo}</div>
         </div>
     )
