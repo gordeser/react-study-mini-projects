e38c62b67a78861b1198d3f6e7c84b9da06a161a
Sergei Gordeev <gordeser@cvut.cz>
2023-02-05 01:34:49 +0100

    Project 23: using local CSS modules

--- 23-css-modules/src/App.js
+++ 23-css-modules/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import Info from "./components/Info";
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+        <Info />
+        <div className="info">
+            <h1>App component heading</h1>
+            <h2>Heading in the App component</h2>
+            <button className="my-button">App component button</button>
+        </div>
+    </div>
   );
 }
 
