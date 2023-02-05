22b60312874388026b7646ca0c12aa9963f50199
Sergei Gordeev <gordeser@cvut.cz>
2023-02-05 17:08:50 +0100

    Project 24: do some css

--- 24-todo-app-v1/src/components/Todos/TodoList.js
+++ 24-todo-app-v1/src/components/Todos/TodoList.js
@@ -1,6 +1,11 @@
 import Todo from "./Todo";
+import styles from './TodoList.module.css'
 function TodoList({todos}) {
-    return todos.length ? todos.map((todo, id) => <Todo key={id} todo={todo}/>) : <h1>TodoList is empty</h1>
+    return(
+        <div className={styles.todoListContainer}>
+            {todos.length ? todos.map((todo, id) => <Todo key={id} todo={todo}/>) : <h2>Todo list is empty</h2>}
+        </div>
+    )
 }
 
 export default TodoList
