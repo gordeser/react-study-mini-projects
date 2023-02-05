717f650f503729d707a23e90760108b46ed26ac1
Sergei Gordeev <gordeser@cvut.cz>
2023-02-05 19:06:02 +0100

    Project 25: complete toggleTodo function

--- 25-todo-app-v2/src/components/Todos/Todo.js
+++ 25-todo-app-v2/src/components/Todos/Todo.js
@@ -2,13 +2,13 @@ import styles from './Todo.module.css'
 import {RiDeleteBin2Line, RiTodoFill} from "react-icons/ri";
 import {FaCheck} from "react-icons/fa";
 
-function Todo({todo, deleteTodo}) {
+function Todo({todo, deleteTodo, toggleTodo}) {
     return (
-        <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
+        <div className={`${styles.todo} ${todo.isCompleted ? styles.completedTodo : ''}`} onDoubleClick={() => deleteTodo(todo.id)}>
             <RiTodoFill className={styles.todoIcon} />
             <div className={styles.todoText}>{todo.text} </div>
-            <RiDeleteBin2Line className={styles.deleteIcon} />
-            <FaCheck className={styles.checkIcon} />
+            <RiDeleteBin2Line className={styles.deleteIcon} onClick={() => deleteTodo(todo.id)} />
+            <FaCheck className={styles.checkIcon} onClick={() => toggleTodo(todo.id)}/>
         </div>
     )
 }
--- 25-todo-app-v2/src/components/Todos/TodoList.js
+++ 25-todo-app-v2/src/components/Todos/TodoList.js
@@ -1,10 +1,10 @@
 import Todo from "./Todo";
 import styles from './TodoList.module.css'
-function TodoList({todos, deleteTodo}) {
+function TodoList({todos, deleteTodo, toggleTodo}) {
     return(
         <div className={styles.todoListContainer}>
             {todos.length
-                ? todos.map((todo) => <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} />)
+                ? todos.map((todo) => <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />)
                 : <h2>Todo list is empty</h2>}
         </div>
     )
