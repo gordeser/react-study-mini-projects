1553610e7969a5e610dda290060fe7319f719438
Sergei Gordeev <gordeser@cvut.cz>
2023-02-06 23:43:46 +0100

    Project 26: add component for each route

---
+++ 26-react-router/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+import NotFound from "./components/NotFound";
+
+function App() {
+  return <BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contacts" element={<Contacts />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  </BrowserRouter>
+}
+
+export default App;
