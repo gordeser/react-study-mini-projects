779ebfe022186f26a425bba43383edc7bd9ad3c1
Sergei Gordeev <gordeser@cvut.cz>
2023-02-06 23:20:12 +0100

    Project 26: add 3 routes to the project

--- 26-react-router/src/App.js
+++ 26-react-router/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App"></div>
-  );
+  return <BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="about" element={<h1>About</h1>} />
+        <Route path="contacts" element={<h1>Contacts</h1>} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Routes>
+    </div>
+  </BrowserRouter>
 }
 
 export default App;
