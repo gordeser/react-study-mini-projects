1f1683b2589bb2c516ab7b1400808d2adee36d84
Sergei Gordeev <gordeser@cvut.cz>
2023-02-07 15:50:29 +0100

    Project 26: handle if sort key is invalid

--- 26-react-router/src/components/Courses.jsx
+++ 26-react-router/src/components/Courses.jsx
@@ -1,11 +1,13 @@
 import courses from "../data/courses";
-import {Link, useLocation} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import queryString from 'query-string'
-import {useState} from "react";
+import {useEffect, useState} from "react";
+
+const SORT_KEYS = ['title', 'slug', 'id']
 
 function sortCourses(courses, key) {
     const sortedCourses = [...courses]
-    if (!key) {
+    if (!key || !SORT_KEYS.includes(key)) {
         return sortedCourses
     }
     sortedCourses.sort((a, b) => a[key] > b[key] ? 1 : -1)
@@ -15,8 +17,16 @@ function sortCourses(courses, key) {
 const Courses = () => {
     const location = useLocation()
     const query = queryString.parse(location.search)
+    const navigate = useNavigate()
     const [sortKey, setSortKey] = useState(query.sort)
     const [sortedCourses, setSortedCourses] = useState(sortCourses(courses, sortKey))
+    useEffect(() => {
+        if (!SORT_KEYS.includes(sortKey)) {
+            navigate('.')
+            setSortKey()
+            setSortedCourses([...sortedCourses])
+        }
+    }, [sortKey, navigate])
 
     return (
         <>
